3d37e7a6d290116ce2cb89c4e48917da05ddfef5
Qalac <officialqalac@gmail.com>
2019-08-19 06:19:52 -0700

    refactored code

--- app.js
+++ app.js
@@ -3,44 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./model/object.model')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/create', (req, res) => {
-    if (!req.body.type) {
+    if (!req.body.type || !req.body.crux || !req.body.color || !req.body.title) {
         return res.status(400).send({
             success: "false",
-            message: 'type is required'
+            input: req.body
         });
     }
-    else if (!req.body.crux) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'crux is required'
-        });
-    }
-    else if (!req.body.color) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'color is required'
-        });
-    }
-    else if (!req.body.title) {
-        return res.status(400).send({
-            success: 'false', 
-            message: 'title is required'
-        });
-    }
+
     else {
         return res.status(201).send({
             success: "true",
             message: "valid",
-            object: db
         });
     }
 });
 
-app.put('/remove/:id', (req, res) => {
+app.patch('/remove/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
+    var attr = req.param('attr')
     let instanceFound;
     let itemIndex;
     db.map((instance, index) => {
@@ -48,20 +31,29 @@ app.put('/remove/:id', (req, res) => {
             instanceFound = instance;
             itemIndex = index;
         }
-    })
-    const updatedInstance = {
-        id: instanceFound.id,
-        type: req.body.type || instanceFound.type,
-        crux: req.body.crux || instanceFound.crux,
-        color: req.body.color || instanceFound.color,
-        title: req.body.title || instanceFound.title
-    };
-    db.splice(itemIndex, 1, updatedInstance);
-    return res.status(201).send({
-        success: "true",
-        message: "successfully updated",
-        db: updatedInstance
     });
+    if (!instanceFound[attr]) {
+        res.status(400).send({
+            success: "false",
+            message: "Attribute not found"
+        });
+    }
+    else {
+        delete instanceFound[attr];
+        const updatedInstance = {
+            id: instanceFound.id,
+            type: instanceFound.type,
+            crux: instanceFound.crux,
+            color: instanceFound.color,
+            title: instanceFound.title
+        };
+        db.splice(itemIndex, 1, updatedInstance);
+        return res.status(201).send({
+            success: "true",
+            message: "successfully updated",
+            db: updatedInstance
+        });
+    }
 });
    
 
